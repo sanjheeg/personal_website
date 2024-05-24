cbec8faa2c047f502c4243f8cf6a3ad9ab3c152c
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-05-24 15:37:48 -0700

    upload skill images

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,15 @@
 import Carousel from "react-multi-carousel";
 import { Container, Col, Row } from "react-bootstrap";
-import "react-multi-carousel/lib/style.css";
-import { skillImage } from "./images/meter1.svg"
+import "react-multi-carousel/lib/styles.css";
+import cadSkill from "./../images/cadSkill.png";
+import cppSkill from "./../images/cppSkill.png";
+import cSkill from "./../images/cSkill.png";
+import pythonSkill from "./../images/pythonSkill.png";
+import swiftSkill from "./../images/swiftSkill.png";
+import shellScriptingSkill from "./../images/shellScriptingSkill.png";
+import x86Skill from "./../images/x86Skill.png";
+import javaSkill from "./../images/javaSkill.png";
+
 
 export const Skills = () => {
     const responsive = {
@@ -30,27 +38,44 @@ export const Skills = () => {
                 <Col>
                 <div className="skill-bx">
                     <h2>
-                        Skills
+                        Skills 
                     </h2>
+                    
                     <p>
                         yap yap yap 
                     </p>
                     <Carousel responsive={responsive} infinite={true} className="skill-slider">
                         <div className="item">
-                            <img src={skillImage} alr="Image"/> 
+                            <img src={cSkill} alt="Image" width="250"/> 
                             <h5>C</h5>
                         </div>
                         <div className="item">
-                            <img src={skillImage} alr="Image"/> 
+                            <img src={javaSkill} alt="Image" width="250"/> 
                             <h5>Java</h5>
                         </div>
                         <div className="item">
-                            <img src={skillImage} alr="Image"/> 
+                            <img src={pythonSkill} alt="Image" width="250"/> 
                             <h5>Python</h5>
                         </div>
                         <div className="item">
-                            <img src={skillImage} alr="Image"/> 
+                            <img src={swiftSkill} alt="Image" width="250"/> 
                             <h5>Swift</h5>
+                        </div>
+                        <div className="item">
+                            <img src={shellScriptingSkill} alt="Image" width="250"/> 
+                            <h5>Shell Scripting</h5>
+                        </div>
+                        <div className="item">
+                            <img src={cppSkill} alt="Image" width="250"/> 
+                            <h5>C++</h5>
+                        </div>
+                        <div className="item">
+                            <img src={x86Skill} alt="Image" width="250"/> 
+                            <h5>X-86 Assembly Language</h5>
+                        </div>
+                        <div className="item">
+                            <img src={cadSkill} alt="Image" width="250"/> 
+                            <h5>CAD</h5>
                         </div>
                     </Carousel>
                 </div>
