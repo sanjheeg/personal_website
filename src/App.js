713c4e3a1957749e54914695b50ae97389dfbe0d
sanjhgup <sanjhgup@cisco.com>
2024-05-24 15:11:05 -0700

    add skills page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./Components/NavBar";
 import { Banner } from "./Components/Banner";
-
+import { Skills } from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
