d6cc92bc01cd133c079ce99c300c75686826c8a0
sanjhgup <sanjhgup@cisco.com>
2024-05-24 09:34:48 -0700

    done with navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,16 @@
 import { useState,useEffect } from "react"
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../images/logo.svg";
+import navIcon1 from "../images/nav-icon1.svg";
+import navIcon2 from "../images/nav-icon2.svg";
+import navIcon3 from "../images/nav-icon3.svg";
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
 
     //see if user has scrolled to change bcknd 
     const [scrolled, seScrolled] = useState(false);
+
     //function that determines what happens upon scroll
     useEffect(() => {
         const onScroll = () => {
@@ -27,7 +32,9 @@ export const NavBar = () => {
     return (
         <Navbar expand = "lg"className = {scrolled ? "scrolled" : ""} >
             <Container>
-                <Navbar.Brand href = "#home"> <img src={''} alt="Logo" /> </Navbar.Brand>
+                <Navbar.Brand href = "#home"> 
+                <img src={logo} alt="Logo" />
+                 </Navbar.Brand>
                 
                 //when window in small shape so image like dotted hamurger
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -44,9 +51,9 @@ export const NavBar = () => {
                     //for social media --linkedin + mail
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={''} alt="" /></a>
-                            <a href="#"><img src={''} alt="" /></a>
-                            <a href="#"><img src={''} alt="" /></a>
+                            <a href="#"><img src={navIcon1} alt="" /></a>
+                            <a href="#"><img src={navIcon2} alt="" /></a>
+                            <a href="#"><img src={navIcon3} alt="" /></a>
                         </div>
                     </span>
                 </Navbar.Collapse>
