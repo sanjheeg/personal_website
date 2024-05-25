6e4ea5e178f31aefd1ff2aadff7aab952423b204
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-05-25 01:25:37 -0700

    add projects

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./Components/NavBar";
 import { Banner } from "./Components/Banner";
 import { Skills } from "./Components/Skills";
+import { Projects } from "./Components/Projects";
 
 function App() {
   return (
