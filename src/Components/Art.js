c96409af5a232c10413fb8fb58c124e3a8ca08f1
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-05-28 16:54:59 -0700

    change item size

--- src/Components/Art.js
+++ src/Components/Art.js
@@ -1,21 +1,28 @@
 import Carousel from "react-multi-carousel";
 import { Container, Col, Row } from "react-bootstrap";
 import "react-multi-carousel/lib/styles.css";
-
+import cadSkill from "./../images/cadSkill.png";
+import cppSkill from "./../images/cppSkill.png";
+import cSkill from "./../images/cSkill.png";
+import pythonSkill from "./../images/pythonSkill.png";
+import swiftSkill from "./../images/swiftSkill.png";
+import shellScriptingSkill from "./../images/shellScriptingSkill.png";
+import x86Skill from "./../images/x86Skill.png";
+import javaSkill from "./../images/javaSkill.png";
 
 export const Art = () => {
     const responsive = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 }, 
-            items: 5
+            items: 1
         },
         desktop: {
             breakpoint: { max: 3000, min: 1024 }, 
-            items: 3
+            items: 1
         },
         tablet: {
             breakpoint: { max: 1024, min: 464 }, 
-            items: 2
+            items: 1
         },
         mobile: {
             breakpoint: { max: 464, min: 0 }, 
@@ -29,16 +36,16 @@ export const Art = () => {
                 <div className="row">
                 <div className="col-12">
                 <div className="skill-bx">
-                    <h2> Skills </h2>
+                    <h2> Art </h2>
                     <p> yap yap yap </p>
                     <Carousel responsive={responsive} infinite={true} className="skill-slider">
                         <div className="item">
                             <img src={cSkill} alt="Image" width="250"/> 
-                            <h5>C</h5>
+                            <h5>Starry Night</h5>
                         </div>
                         <div className="item">
                             <img src={javaSkill} alt="Image" width="250"/> 
-                            <h5>Java</h5>
+                            <h5>Dogs in a backpack</h5>
                         </div>
                         <div className="item">
                             <img src={pythonSkill} alt="Image" width="250"/> 
