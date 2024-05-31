fcfe17052d988d82f93b4fc2ec53ce8bd11f369a
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-05-31 09:23:00 -0700

    change images

--- src/Components/Baking.js
+++ src/Components/Baking.js
@@ -38,35 +38,35 @@ export const Baking = () => {
                     <p> Outside of my interest in the STEM field, I love unleashing my creativity by baking. It helps me relieve stress and the end result is so sweet (quite literally!). I make all of these from scratch (meaning no box mixes) and don't use eggs </p>
                     <Carousel responsive={responsive} infinite={true} className="skill-slider">
                         <div className="item">
-                            <img src={cSkill} alt="Image" width="250"/> 
+                            <img src={mangoLassi} alt="Image"/> 
                             <h5>Tiramisu</h5>
                         </div>
                         <div className="item">
-                            <img src={javaSkill} alt="Image" width="250"/> 
+                            <img src={mangoLassi} alt="Image" /> 
                             <h5>Peanut Butter and Chocolate Cake</h5>
                         </div>
                         <div className="item">
-                            <img src={mangoLassi} alt="Image" width="250"/> 
+                            <img src={mangoLassi} alt="Image"/> 
                             <h5>Mango Lassi Cheesecake</h5>
                         </div>
                         <div className="item">
-                            <img src={vdayCupcakes} alt="Image" width="250"/> 
+                            <img src={vdayCupcakes} alt="Image" /> 
                             <h5>Valentine's Day Cupcakes</h5>
                         </div>
                         <div className="item">
-                            <img src={turkeyCupcakes} alt="Image" width="250"/> 
+                            <img src={turkeyCupcakes} alt="Image" /> 
                             <h5>Turkey Cupcakes</h5>
                         </div>
                         <div className="item">
-                            <img src={christCookies} alt="Image" width="250"/> 
+                            <img src={christCookies} alt="Image" /> 
                             <h5>Christmas Cookies</h5>
                         </div>
                         <div className="item">
-                            <img src={springCookies} alt="Image" width="250"/> 
+                            <img src={springCookies} alt="Image" /> 
                             <h5>Spring break cookies</h5>
                         </div>
                         <div className="item">
-                            <img src={cadSkill} alt="Image" width="250"/> 
+                            <img src={mangoLassi} alt="Image" /> 
                             <h5>Chocolate Fruit Cake</h5>
                         </div>
                     </Carousel>
