d3a8ead292cc452f88e6583c652626055c7abe0d
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2024-05-31 14:26:48 -0700

    add images

--- src/Components/Baking.js
+++ src/Components/Baking.js
@@ -2,7 +2,11 @@ import Carousel from "react-multi-carousel";
 import { Container, Col, Row } from "react-bootstrap";
 import "react-multi-carousel/lib/styles.css";
 import vdayCupcakes from "./../images/vdayCupcakes.jpeg";
-import mangoLassi from "./../images/mangoLassi1.jpeg";
+import mangoLassi from "./../images/mangoLassi.png";
+import tiramisu from "./../images/tiramisu.png";
+import peanutButter from "./../images/peanutButter.png";
+import pinkLemonade from "./../images/pinkLemonade.png";
+import fruitCake from "./../images/fruitCake.jpeg";
 import turkeyCupcakes from "./../images/turkeyCupcakes.jpeg";
 import christCookies from "./../images/christCookies.jpeg";
 import springCookies from "./../images/springCookies.jpeg";
@@ -36,13 +40,14 @@ export const Baking = () => {
                 <div className="skill-bx">
                     <h2> Baking </h2>
                     <p> Outside of my interest in the STEM field, I love unleashing my creativity by baking. It helps me relieve stress and the end result is so sweet (quite literally!). I make all of these from scratch (meaning no box mixes) and don't use eggs </p>
+                    <p></p>
                     <Carousel responsive={responsive} infinite={true} className="skill-slider">
                         <div className="item">
-                            <img src={mangoLassi} alt="Image"/> 
+                            <img src={tiramisu} alt="Image"/> 
                             <h5>Tiramisu</h5>
                         </div>
                         <div className="item">
-                            <img src={mangoLassi} alt="Image" /> 
+                            <img src={peanutButter} alt="Image" /> 
                             <h5>Peanut Butter and Chocolate Cake</h5>
                         </div>
                         <div className="item">
@@ -66,8 +71,12 @@ export const Baking = () => {
                             <h5>Spring break cookies</h5>
                         </div>
                         <div className="item">
-                            <img src={mangoLassi} alt="Image" /> 
+                            <img src={fruitCake} alt="Image" /> 
                             <h5>Chocolate Fruit Cake</h5>
+                        </div>
+                        <div className="item">
+                            <img src={pinkLemonade} alt="Image" /> 
+                            <h5>Pink Lemonade Cake</h5>
                         </div>
                     </Carousel>
                 </div>
