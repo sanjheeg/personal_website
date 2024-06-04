3ecc052733e4e2e545de2c10d1eb99b9f3b89f51
sanjhgup <sanjhgup@cisco.com>
2024-06-04 15:47:48 -0700

    add test text

---
+++ src/Components/Experience.js
@@ -0,0 +1,26 @@
+
+import { Container, Col, Row } from "react-bootstrap";
+import "react-multi-carousel/lib/styles.css";
+
+
+
+export const Experience = () => {
+    return (
+        <section className="experience" id="experience">
+            <div className="container">
+                <div className="row">
+                <div className="col-12">
+                <div className="skill-bx">
+                    <h2> Experience </h2>
+                    <h3>Sofware Engineering Intern </h3>
+                    <h4>Cisco</h4>
+                    <p>- work with people</p>
+
+                </div>
+                </div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
