6bd42b9e7a3b3563af46f9d586c2567a4f770772
sanjhgup <sanjhgup@cisco.com>
2024-06-05 13:58:21 -0700

    add experience text

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -12,13 +12,29 @@ export const Experience = () => {
                 <div className="col-12">
                 <div className="skill-bx">
                     <h2> Experience </h2>
+
                     <h3>Sofware Engineering Intern </h3>
                     <h4>Cisco - San Jose, California (on-site)</h4>
                     <p>- work with people</p>
+
                     <h3>Director</h3>
                     <h4>BoilerMake - West Lafayette, Indiana (on-site)</h4>
-                    <p>- work with people</p>
-
+                    <p>- Lead and manage a team of 32 undergraduate students to enhance and guarantee the successful execution of the campus's biggest hackathon, spanning three consecutive days</p>
+                    <p>- Collaborate across Purdue University departments to streamline event operations for maximum efficiency</p>
+                    
+                    <h3>Undergraduate Head Teaching Assisstant</h3>
+                    <h4>The Data Mine - West Lafayette, Indiana (on-site)</h4>
+                    <p>- Actively engage with students as a Teaching Assistant for TDM101 and TDM102, offering guidance and support.</p>
+                    <p>- Answered over 500 questions posted by students on Piazza, the online forum used by the TDM classes, during the Spring 2024 semester.</p>
+                    <p>- Conduct regular office hours, providing one-on-one and group assistance to students working on weekly projects involving R, Python, Bash, and SQL.</p>
+                    <p>- Review and assess projects in advance, proactively identifying potential issues and devising effective solutions.</p>
+                
+                    <h3>Undergraduate Data Science Researcher</h3>
+                    <h4>The Data Mine - West Lafayette, Indiana (on-site)</h4>
+                    <p>- Collaborated with Renzoe Box to improve their color smart matching program.</p>
+                    <p>- Conducted web scraping to gather essential data, utilizing it to train advanced algorithms aimed at precise customer skin-tone matching with Renzoe Box foundation products.</p>
+                    <p>- Worked closely within a 10-member team to efficiently complete assigned tasks, ensuring successful project delivery.</p>
+                
                 </div>
                 </div>
                 </div>
