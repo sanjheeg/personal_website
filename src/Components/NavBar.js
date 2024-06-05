e0492bfc761892e2b4fa794be8fc2605713f24db
sanjhgup <sanjhgup@cisco.com>
2024-06-05 14:16:34 -0700

    test if link works

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -36,7 +36,6 @@ export const NavBar = () => {
                 <img src={logo} alt="Logo" />
                  </Navbar.Brand>
                 
-                
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggle-icon"></span>
                 </Navbar.Toggle>
@@ -54,7 +53,9 @@ export const NavBar = () => {
                     
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#"><img src={navIcon1} alt="" /></a>
+                            <a href="#" onClick={(e) => { e.preventDefault(); handleIconClick('https://www.linkedin.com/in/sanjhee-gupta/'); }}>
+                                <img src={navIcon1} alt="" />
+                            </a>    
                             <a href="#"><img src={navIcon2} alt="" /></a>
                             <a href="#"><img src={navIcon3} alt="" /></a>
                         </div>
